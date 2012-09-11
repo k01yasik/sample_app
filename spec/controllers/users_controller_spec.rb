e28639e0a07b3284e42d72d4d8fc26a7c2aae021
Nickolay A. Smirnov <bzdykin@mail.ru>
2012-09-11 09:00:34 +0400

    Finish

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe UsersController do
-
+	render_views
+	
   describe "GET 'new'" do
-    it "returns http success" do
+    it "should be success" do
       get 'new'
       response.should be_success
     end
+	
+  it "should have tht right title" do
+	get 'new'
+	response.should have_selector("title", :content => "Sign up")
+  end	
+  
   end
 
 end
