acfdd27ef32af46a69d0d8e8c20d849fa80c7fa3
Nickolay A. Smirnov <bzdykin@mail.ru>
2012-09-19 16:31:36 +0400

    FFFJL

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -198,5 +198,34 @@ end
 			response.should redirect_to(signin_path)
 		end
 	end
+	
+	describe "for signed-in users" do
+	
+		before(:each) do
+			wrong_user = FactoryGirl.create(:user, :email => "user@example.net")
+			test_sign_in(wrong_user)
+		end
+
+		it "should require matching users for 'edit'" do
+			get :edit, :id => @user
+			response.should redirect_to(root_path)
+		end
+
+		it "should require matching users for 'update'" do
+			put :update, :id => @user, :user => {}
+			response.should redirect_to(root_path)
+		end
+	end	
   end	
+  
+  describe "GET 'index'" do
+  
+	describe "for non-signed-in users" do
+		it "should deny access" do
+			get :index
+			response.should redirect_to(signin_path)
+			flash[:notice].should =~ /sign in/i
+		end	
+	end	
+  end
 end
